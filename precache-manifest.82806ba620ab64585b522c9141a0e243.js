34ddd3d9fa6818dfa2f0102f7fa720eeea0c2cfb
Code Sandbox <hello@codesandbox.io>
2022-07-07 20:56:25 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.82806ba620ab64585b522c9141a0e243.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b45eeb0e75c8a90dcda8fd87c1b8303b",
+    "url": "https://victoriendotnet.github.io/csb-lbrlud/index.html"
+  },
+  {
+    "revision": "d0c9daabaf0019b1a575",
+    "url": "https://victoriendotnet.github.io/csb-lbrlud/static/css/main.ca4dcd7a.chunk.css"
+  },
+  {
+    "revision": "cafee588fc2679ba4a23",
+    "url": "https://victoriendotnet.github.io/csb-lbrlud/static/js/2.66be449f.chunk.js"
+  },
+  {
+    "revision": "d0c9daabaf0019b1a575",
+    "url": "https://victoriendotnet.github.io/csb-lbrlud/static/js/main.e7c7f847.chunk.js"
+  },
+  {
+    "revision": "278b49005cc26a39f60b",
+    "url": "https://victoriendotnet.github.io/csb-lbrlud/static/js/runtime~main.adcf65ee.js"
+  }
+]);
